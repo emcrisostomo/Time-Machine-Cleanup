1f7ad05fde8d1971ee4bca12816f3be99e187e5e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2015-05-02 13:49:13 +0200

    Update license.

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,20 @@
 #!/bin/zsh
 # -*- coding: utf-8; tab-width: 2; indent-tabs-mode: nil; sh-basic-offset: 2; sh-indentation: 2; -*- vim:fenc=utf-8:et:sw=2:ts=2:sts=2
 #
-# Copyright (C) 2015, Grey Systems
-# All rights reserved.
+# Copyright (C) 2015, Enrico M. Crisostomo
 #
-# Unauthorized copy and distribution of this file is strictly prohibited.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 setopt localoptions
 setopt localtraps
